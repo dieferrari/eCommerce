0daa5dd376580b69c334a5ca3463645b9bd52f30
dieferrari <die.a.ferrari@gmail.com>
2018-03-06 19:21:30 -0300

    front routes tested

--- front/src/components/App.js
+++ front/src/components/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { Route,Switch } from 'react-router-dom';
 import RegisterContainer from '../containers/register-container';
 import LoginContainer from '../containers/login-container';
+import ProductsContainer from '../containers/ProductsContainer';
+import SingleProductContainer from '../containers/SingleProductContainer';
+import CategoryContainer from '../containers/CategoryContainer';
 
 export default () => (
     <div>
@@ -11,6 +14,22 @@ export default () => (
           path="/register"
           component={RegisterContainer}
         />
+        <Route
+          exact 
+          path="/products"
+          component={ProductsContainer}
+        />
+         <Route 
+          path="/products/:id"
+          component={SingleProductContainer}
+        />
+          <Route
+          exact 
+          path="/category"
+          component={CategoryContainer}
+        />
+          
+
         {/* <Route
           exact
           path="/login"
