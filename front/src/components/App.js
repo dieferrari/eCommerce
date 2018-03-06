87f0d3368d61d48d3e7490b606d46c3c66fedcdf
Giovanny <eddy.rivera.echegaray@gmail.com>
2018-03-06 18:47:15 -0300

    fix-app.js

--- front/src/components/App.js
+++ front/src/components/App.js
@@ -12,11 +12,11 @@ export default () => (
           path="/register"
           component={RegisterContainer}
         />
-        {/* <Route
+        {/* /* <Route
           exact
           path="/login"
           component={LoginContainer}
-        />
+        /> */}
 
 
       </Switch>
--- front/src/redux/actions/user.js
+++ front/src/redux/actions/user.js
@@ -6,13 +6,8 @@ const receiveUser = (user) => ({
     user,
   });
 
-<<<<<<< HEAD
-export const createUser = user => dispatch =>
-  axios.post(`localhost:3005/register`, user)
-=======
-export const createUser = user => dispatch => axios
-    .post(`http://localhost:3005/users/register`, user)
->>>>>>> c551ff7eeb0e4f59346f0b7581f8c6bd68bf5a06
+export const createUser = user => dispatch => 
+    axios.post(`http://localhost:3005/users/register`, user)
     .then(res => res.data)
     .then(createdUser => {
       dispatch(receiveUser(createdUser))});
