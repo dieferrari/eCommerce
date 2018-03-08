aef8d211a576c9327da6d35c373912d4c76709eb
Nicolas Sieiro <sieiro.nicolas@gmail.com>
2018-03-08 10:12:44 -0300

    Revert "creando reviews"

--- back/routes/products.js
+++ back/routes/products.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { Categories, Product, Reviews } = require('../models/');
+const { Categories, Product } = require('../models/');
 
 router.get('/', function (req, res, next) {
-    Product.findAll({ include: [ Categories, Reviews] })
+    Product.findAll({ include: [ Categories ] })
         .then((products) => res.status(200).send(products))
 })
 router.post('/',function(req,res,next){
@@ -12,7 +12,7 @@ router.post('/',function(req,res,next){
 })
 router.get('/:id',function(req,res,next){
     Product.findById(req.params.id,{
-        include: [Categories, Reviews]
+        include: [Categories]
     })
         .then((product) => res.status(200).send(product))
         .catch((err) => res.send(err))
--- back/seeders/index.js
+++ back/seeders/index.js
@@ -1,8 +1,9 @@
-const {User, Categories, Product, Reviews} =require('../models');
+const {User, Categories, Product} =require('../models');
 const productos=require('./products-seed');
 const usuarios=require('./user-seed');
-const categorias=require('./categories-seed');
-const reviews=require('./reviews-seed');
+const categorias=require('./categories-seed')
+
+
 
 
 module.exports=function(){
@@ -36,13 +37,6 @@ module.exports=function(){
     }).then(usuario=>{
         return usuario.addProducts([1,2,3])
     }).then(()=>{
-        return Reviews.bulkCreate(reviews)
-    }).then(()=> {
-        return Product.findById(2)
-    })
-    .then((producto)=>{
-        return producto.addReviews([1,3,5])
-    }).then(()=>{
-        console.log("PiNgO")
+        console.log('Tomi TermiChapo')
     })
 }
