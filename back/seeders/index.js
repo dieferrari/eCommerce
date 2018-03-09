030fbb90f067afc46d0114cfbce11711e1106930
FSBalbuena <fsbalbuena@gmail.com>
2018-03-09 12:50:49 -0300

    guardo mis seeders etc

--- back/seeders/index.js
+++ back/seeders/index.js
@@ -19,8 +19,20 @@ module.exports=function(){
     })
     .then(order=>{
         return order.addProducts([102],{ through: { cantidad: 6 }})
+    })
+    .then(()=>{
+        return Orders.findById(101)
+    })
+    .then(order=>{
+        return order.addProducts([101],{ through: { cantidad: 3 }})
+    })
+    .then(()=>{
+        return Orders.findById(101)
+    })
+    .then(order=>{
+        return order.addProducts([103],{ through: { cantidad: 1 }})
     }).then(()=>{
-        return Product.findById(101)
+        return Product.findById(102)
     })
     .then(producto=>{
         return producto.addCategories([105])
---
+++ front/src/components/OrdersByUser.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default ({ products }) => {
+      
+      var sumTotal = products.reduce(function(tot, record) {
+          return tot + record.orderProduct.cantidad * record.price;
+      },0);
+    
+    return(<div>
+      {!products ? "Loading" :
+      <div>
+      {products.map(product=>(
+        <p key={product.id}>
+            <Link to={`/products/${product.id}`}>
+              <p>Product Name:{product.name}</p>   
+              <div>
+                <img src={product.imgURL} alt=""/>  
+              </div> 
+            </Link>
+            <p>Precio: {product.price}</p>
+            <p>Cantidad: {product.orderProduct.cantidad}</p>
+            <p>Total: {product.orderProduct.cantidad * product.price}</p>
+        </p>
+      ))}
+      <p>Precio Final: {sumTotal}</p>
+      </div>}
+    </div>)
+};
